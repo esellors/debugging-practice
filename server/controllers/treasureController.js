7f8f44960a78aa0d988b163f81e4a581e3439459
Eric <github@esellors.com>
2019-06-06 19:24:06 -0500

    step 7 part 1 complete

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -9,5 +9,13 @@ module.exports = {
       console.log('hit')
       const treasure = await req.app.get('db').get_user_treasure(req.session.user.id);
       res.status(200).send(treasure);
+   },
+   addUserTreasure: async (req, res) => {
+      console.log('hit')
+      const {treasureURL} = req.body;
+      const {id} = req.session.user;
+
+      const userTreasure = await req.app.get('db').add_user_treasure(treasureURL, id)
+      res.status(200).json(userTreasure);
    }
 }
--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ app.get('/auth/logout', authCtrl.logout);
 
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
 app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
 
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`));
